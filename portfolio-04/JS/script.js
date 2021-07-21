360d6697c61b979a0aed49506390f8067959171d
SHINJI KAWAZU <k2merci@gmail.com>
2021-07-22 08:24:02 +0900

    2021/7/22　不具合修正

--- portfolio-04/JS/script.js
+++ portfolio-04/JS/script.js
@@ -1,55 +1,22 @@
 $(function(){
 
-  $(function($){
-    $('.headerImg').bgSwitcher({
-      images:['img/fv-bgi_01@2x.jpg','img/fv-bgi_02@2x.jpg','img/fv-bgi_03@2x.jpg'],
-    });
-  });
-
-  $(window).on('scroll',function(){
-    if($('#header').height() < $(this).scrollTop()){
-      $('.headerNav').addClass('withColor');
-    }else{
-      $('.headerNav').removeClass('withColor');
-    }
-  
-  });
-
-
-  $(window).on('load',function(){
-    $('body').removeClass('fadeout');
-  });
-
-  $('a:not([href^="#"]):not([target])').on('click',function(e){
-    e.preventDefault();
-    url=$(this).attr('href');
-    if(url !== ''){
-      $('body').addClass('fadeout');
-      setTimeout(function(){
-        window.location = url;
-      },400);
-    }
-    return false;
-  });
-
-  $('.hamburgerMenu').click(function(){
-    $('.hamburgerMenu').toggleClass('close');
-  // クラスの付与で、アイコン切り替え
-   $('.hamburger').fadeToggle();
-      // メニューの表示
-      $('body,html').scrollTop(0).toggleClass('noScroll')
-      //ページを一番上に戻す＋スクロール防止
-  });
-  
-  
-  /*aタグ要素に対して*/
-  
-  $('a').click(function(){
-    $('.hamburger').hide();
-    // リンクをクリックしたらメニューを消す
-    $('.hamburgerMenu.close').removeClass('close');
-    // バーガーメニューをもとに戻す
-    $('body.noScroll').removeClass('noScroll')
-    // スクロール防止を消す
-  });
+	$(window).scroll(function(){
+	
+		if($(window).scrollTop() > $("#concept").offset().top){
+		
+		$(".hero-area__navigation").css("background","#232F35");
+		
+		}else{
+		$(".hero-area__navigation").css("background","none");
+		
+		}
+	});
+	
+	
+	$("a").click(function(){
+		$("#menu-btn").removeAttr("checked").prop('checked', false).change();
+		
+	});
+	
 });
+
